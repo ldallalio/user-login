0a0f9f5953dce3f9c75d95ab646106a04a48bf40
Logan Dallalio <logan@dallalioweb.dev>
2022-02-02 03:20:53 -0600

    updated about and contact

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function About() {
-	return <div>About</div>;
+	return (
+		<div className='container-fluid mt-5'>
+			<div className='container-md '>
+				<div className='hero  shadow-lg user-login' id='homeDiv'>
+					<h2 className='text-center text-nowrap mb-5'>About - UserLoginApp</h2>
+					<p>
+						This app is created with ReactJs and Bootstrap to enable users to
+						sign-up and sign in using Firebase.
+					</p>
+					<a
+						href='https://dallalioweb.dev'
+						className='link link-success text-sm-center'>
+						Developed by: Logan || Dallalio Web Development
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 function Contact() {
-	return <div>Contact</div>;
+	const handleSubmit = (e) => {
+		e.preventDeafult();
+	};
+	return (
+		<div className='container-fluid mt-5 user-login'>
+			<div className='container-md '>
+				<form className='' onSubmit={handleSubmit}>
+					<label className='mb-2' htmlFor='email'>
+						Email Address
+					</label>
+					<input
+						className='form-control mb-2'
+						id='email'
+						type='email'
+						name='email'
+					/>
+
+					<label className='mb-2' htmlFor='message'>
+						Message
+					</label>
+					<textarea className='form-control mb-4' id='message' name='message' />
+
+					<button
+						type='submit'
+						className='form-control btn btn-primary btn-block mb-8'>
+						Submit
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Contact;
